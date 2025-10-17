97383924edc63d5df1ccd1ecafa9f35dc4a76568
syaqilazadira-glitch <syaqilazadira@gmail.com>
2025-10-18 02:14:05 +0700

    wanzofc

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,17 @@
-// src/config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://zanssxploit:pISqUYgJJDfnLW9b@cluster0.fgram.mongodb.net/suntik?retryWrites=true&w=majority");
-        console.log('MongoDB Connected Successfully!');
-    } catch (err) {
-        console.error('MongoDB Connection Error:', err.message);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    await mongoose.connect("mongodb+srv://zanssxploit:pISqUYgJJDfnLW9b@cluster0.fgram.mongodb.net/suntik?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // timeout 10 detik
+    });
+    console.log("✅ MongoDB Connected...");
+  } catch (err) {
+    console.error("❌ Database connection failed:", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
